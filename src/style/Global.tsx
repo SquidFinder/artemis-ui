34873500b67bc1f15f292a37d86ee9349c81f160
Ignacio Freijomil <artemisprotocol@gmail.com>
2021-10-06 18:35:58 -0300

    test

--- src/style/Global.tsx
+++ src/style/Global.tsx
@@ -2,15 +2,20 @@ import { createGlobalStyle } from 'styled-components'
 // eslint-disable-next-line import/no-unresolved
 import { PancakeTheme } from '@pancakeswap-libs/uikit'
 
+
 declare module 'styled-components' {
   /* eslint-disable @typescript-eslint/no-empty-interface */
   export interface DefaultTheme extends PancakeTheme {}
+  
 }
+
+
+
 
 const GlobalStyle = createGlobalStyle`
   * {
-    font-family: 'Inter, sans-serif' !important;
-    font-weight: 700;
+    font-family: 'Inter', sans-serif !important;
+    font-weight: 510;
     
   }
 
