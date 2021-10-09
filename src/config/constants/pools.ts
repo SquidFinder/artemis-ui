98057cba69edda617ea4063e0f7636800b9c20de
snappycreeper <artemisprotocol@gmail.com>
2021-10-09 10:28:17 -0500

    Update pools.ts

--- src/config/constants/pools.ts
+++ src/config/constants/pools.ts
@@ -1,41 +1,50 @@
 import { PoolConfig, QuoteToken, PoolCategory } from './types'
 
 const pools: PoolConfig[] = [
-  /* {
-    tokenName: 'WONE',
-    stakingTokenName: QuoteToken.MISONE,
-    stakingTokenAddress: '0x31222d37568BD54be95eCE3749f0559c45a85634',
+  {
+    tokenName: 'XYA',
+    stakingTokenName: QuoteToken.MISXYA,
+    stakingTokenAddress: '0xe22297cc3452aae66cee6ed1cb437e96219c3319',
+     // tokenPoolAddress is MIS
+    tokenPoolAddress: '0xD74433B187Cf0ba998Ad9Be3486B929c76815215',
+     // quoteTokenPoolAddress is XYA
+    quoteTokenPoolAddress: '0x9b68bf4bf89c115c721105eaf6bd5164afcc51e4',
     contractAddress: {
-      1666700000: '0x736E9c99cb9f9c39e25eF61181eeC83d484F9120',
-      1666600000: '0xc078b878ae6BB14a8734d8a788F496984Cc7dde2',
+      1666700000: '0x5aaf4bbECFf610186aeA98218f697029D5a77597',
+      1666600000: '0x5aaf4bbECFf610186aeA98218f697029D5a77597',
     },
-    poolCategory: PoolCategory.CORE,
-    projectLink: 'https://www.harmony.one/',
+    poolCategory: PoolCategory.COMMUNITY,
+    projectLink: 'https:www.freyala.com/',
     harvest: true,
-    tokenPerBlock: '1',
+    tokenPerBlock: '0.16',
     sortOrder: 1,
     isFinished: false,
-    startBlock: 15499711,
-    endBlock: 20499711,
+    startBlock: 17953943,
+    endBlock: 18554800,
     tokenDecimals: 18,
-   }, */
-  // {
-  //   sousId: 1,
-  //   tokenName: 'TWT',
-  //   stakingTokenName: QuoteToken.SYRUP,
-  //   stakingTokenAddress: '0x009cF7bC57584b7998236eff51b98A168DceA9B0',
-  //   contractAddress: {
-  //     1666700000: '0xAfd61Dc94f11A70Ae110dC0E0F2061Af166660000033061A',
-  //     1666600000: '0xAfd61Dc94f11A70Ae110dC0E0F2061Af166660000033061A',
-  //   },
-  //   poolCategory: PoolCategory.CORE,
-  //   projectLink: 'https://trustwallet.com/',
-  //   harvest: true,
-  //   tokenPerBlock: '20',
-  //   sortOrder: 999,
-  //   isFinished: true,
-  //   tokenDecimals: 18,
-  // },
+   },
+   {
+    tokenName: 'RAIN',
+    stakingTokenName: QuoteToken.MISUST,
+    stakingTokenAddress: '0xfc417a0368263140c59b7aab646d4a270c37d8cb',
+     // tokenPoolAddress is MIS
+    tokenPoolAddress: '0xD74433B187Cf0ba998Ad9Be3486B929c76815215',
+     // quoteTokenPoolAddress is UST
+    quoteTokenPoolAddress: '0x224e64ec1bdce3870a6a6c777edd450454068fec',
+    contractAddress: {
+      1666700000: '0x57e211444a35223649Fa988e80A9E5d594D05250',
+      1666600000: '0x57e211444a35223649Fa988e80A9E5d594D05250',
+    },
+    poolCategory: PoolCategory.COMMUNITY,
+    projectLink: 'https:www.freyala.com/',
+    harvest: true,
+    tokenPerBlock: '0.01',
+    sortOrder: 2,
+    isFinished: false,
+    startBlock: 17971626,
+    endBlock: 18971226,
+    tokenDecimals: 18,
+   },
 ]
 
 export default pools
