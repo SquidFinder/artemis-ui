7348fd279474ca5d2346f6df6d3658c0908c6fd8
snappycreeper <artemisprotocol@gmail.com>
2021-10-10 22:46:28 -0500

    Update pools.ts

--- src/config/constants/pools.ts
+++ src/config/constants/pools.ts
@@ -7,9 +7,9 @@ const pools: PoolConfig[] = [
     quoteTokenSymbol: QuoteToken.MIS,
     stakingTokenName: QuoteToken.MISTRANQ,
     stakingTokenAddress: '0xfA45e64Adf9BF3caDC65b737b2B0151C750f414C',
-    // this is TRANQ
-    quoteTokenPoolAddress: '0xd74433b187cf0ba998ad9be3486b929c76815215',
     // this is MIS
+    quoteTokenPoolAddress: '0xd74433b187cf0ba998ad9be3486b929c76815215',
+    // this is TRANQ
     tokenPoolAddress: '0xcf1709ad76a79d5a60210f23e81ce2460542a836',
     contractAddress: {
       1666700000: '0xC2B1FC77e49BE0a99520EAd73da3a76AF533482D',
@@ -18,7 +18,7 @@ const pools: PoolConfig[] = [
     poolCategory: PoolCategory.COMMUNITY,
     projectLink: 'https://www.tranquil.finance/',
     harvest: true,
-    tokenPerBlock: '0.22',
+    tokenPerBlock: '0.21',
     sortOrder: 1,
     isFinished: false,
     startBlock: 17996500,
@@ -32,9 +32,9 @@ const pools: PoolConfig[] = [
     stakingTokenName: QuoteToken.MISXYA,
     stakingTokenAddress: '0xE22297CC3452aae66cEE6ED1cb437e96219c3319',
      // this is MIS
-     tokenPoolAddress: '0x9b68bf4bf89c115c721105eaf6bd5164afcc51e4',
-     // this is XYA
      quoteTokenPoolAddress: '0xd74433b187cf0ba998ad9be3486b929c76815215',
+     // this is XYA
+     tokenPoolAddress: '0x9b68bf4bf89c115c721105eaf6bd5164afcc51e4',
     contractAddress: {
       1666700000: '0x5aaf4bbECFf610186aeA98218f697029D5a77597',
       1666600000: '0x5aaf4bbECFf610186aeA98218f697029D5a77597',
