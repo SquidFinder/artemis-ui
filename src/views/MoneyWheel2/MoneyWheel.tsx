6e1b12751c6d5055494740a65ad46faf549b64de
Ignacio Freijomil <artemisprotocol@gmail.com>
2021-10-20 08:23:48 -0300

    Earn UI Improvs.

--- src/views/MoneyWheel2/MoneyWheel.tsx
+++ src/views/MoneyWheel2/MoneyWheel.tsx
@@ -1,13 +1,40 @@
-import React, {useState} from 'react'
+import React, {useCallback, useState} from 'react'
 import styled from 'styled-components'
-import { BaseLayout, Card, CardBody, Link, LinkExternal  } from '@pancakeswap-libs/uikit'
+import { BaseLayout, Button, Card, CardBody, Link, LinkExternal  } from '@pancakeswap-libs/uikit'
 import BigNumber from 'bignumber.js'
 import Page from 'components/layout/Page'
 import useTokenBalance from 'hooks/useTokenBalance'
-import { FaUserCheck, FaLock, FaHistory, FaScroll, FaMonument, FaCloud, FaFire, FaMapMarkedAlt, FaMap } from 'react-icons/fa';
+import { FaUserCheck, FaLock, FaHistory, FaScroll, FaMonument, FaCloud, FaFire, FaMapMarkedAlt, FaMap, FaUserClock } from 'react-icons/fa';
 import HomePage from 'components/layout/HomePage'
+import { usePriceCakeBusd } from 'state/hooks'
+import { useAllHarvest } from 'hooks/useHarvest'
+import { useWallet } from '@binance-chain/bsc-use-wallet'
+import useFarmsWithBalance from 'hooks/useFarmsWithBalance'
+import { getBalanceNumber } from 'utils/formatBalance'
+import useI18n from 'hooks/useI18n'
+import CakeHarvestBalance from 'views/Home/components/CakeHarvestBalance'
+import UnlockButton from 'components/UnlockButton'
+import CakeWalletBalance from 'views/Home/components/CakeWalletBalance'
 import { getCakeAddress, getOneAddress } from '../../utils/addressHelpers'
 
+const Price = styled.button`
+-webkit-box-align: center;
+align-items: center;
+background-color: rgba(0, 0, 0,0) !important;
+border: 1px;
+border-style: solid !important;
+border-color: #405fb4 !important;
+border-radius: 16px;
+color: #405fb4;
+font-size: 15px;
+font-weight: 800;
+width: 100%;
+display: inline-flex;
+min-height: 21px;
+max-height: 37px;
+letter-spacing: 0.03em;
+padding: 15px;
+`
 
 const Divider = styled.div`
 background-color: #FAFAFA;
@@ -19,6 +46,33 @@ margin-bottom: 5px;
 width: 100%;
 `
 
+const StyledFarmStakingCard = styled(Card)`
+  background-repeat: no-repeat;
+  background-position: top right;
+  border-radius: 14px;
+`
+
+const Block = styled.div`
+  margin-bottom: 5px;
+`
+
+const CardImage = styled.img`
+  margin-bottom: 5px;
+`
+
+const Label = styled.div`
+  color: ${({ theme }) => theme.colors.textSubtle};
+  font-size: 12px !important;
+`
+
+const Actions = styled.div`
+  margin-top: 12px;
+`
+
+const FlowCol = styled.div`
+  display: flex;
+  flex-flow: column;
+`
 
 const StyledCakeStats = styled(Card)`
   margin-left: auto;
@@ -110,21 +164,107 @@ const SvgHero = styled.div`
 `
   
 const MoneyWheel: React.FC = () => {
-  
+  const [pendingTx, setPendingTx] = useState(false)
+  const { account } = useWallet()
+  const TranslateString = useI18n()
+  const farmsWithBalance = useFarmsWithBalance()
+  const cakeBalance = getBalanceNumber(useTokenBalance(getCakeAddress()))
+  const eggPrice = usePriceCakeBusd().toNumber()
+  const earningsSum = farmsWithBalance.reduce((accum, farm) => {
+    return accum + new BigNumber(farm.balance).div(new BigNumber(10).pow(18)).toNumber()
+  }, 0)
+  const balancesWithValue = farmsWithBalance.filter((balanceType) => balanceType.balance.toNumber() > 0)
+
+  const { onReward } = useAllHarvest(balancesWithValue.map((farmWithBalance) => farmWithBalance.pid))
+
+  const harvestAllFarms = useCallback(async () => {
+    setPendingTx(true)
+    try {
+      await onReward()
+    } catch (error) {
+      // TODO: find a way to handle when the user rejects transaction or it fails
+    } finally {
+      setPendingTx(false)
+    }
+  }, [onReward])
+
+
   return (
     <Page>
 
 
 
+
+
     <StyledCakeStats style={{"boxShadow":"0px 2px 12px -8px rgba(25, 19, 38, 0.1), 0px 1px 1px rgba(25, 19, 38, 0.05)"}}>
       <CardBody>
-        
+      
     <div>
     <SvgHero>
       <object type="image/svg+xml" data="images/artemisearn.svg" width="600px">&nbsp;</object>
+      <CardBody>
+
+          <FlowCol>
+         
+
+                  <Sub className="nav-links connect">
+                  { account != null && account.length > 1? 
+                    <Price>{account.substring(0,8).concat("...")} <p style={{'color': '#4c68ef'}}> ✓</p></Price>:
+                  <UnlockButton style={{
+                    backgroundColor: 'rgb(22, 35, 73) !important',
+                    border: '0px',
+                    color: '#8299dd !important',
+                    borderRadius: '15px',
+                    fontSize: '15px',
+                    fontWeight: '800',
+                    width: '100%',
+                    display: 'inline-flex',
+                    letterSpacing: '0.03em',
+                    padding: '15px',
+                    flexFlow: 'row',
+                    alignItems: 'center',
+                    justifyContent: 'space-between',
+                    'minHeight':'21px',
+                    'maxHeight':'37px'
+                  }}>Connect</UnlockButton>
+                  }
+                  </Sub>
+
+
+
+            <div style={{'display':'inline-block', 'paddingLeft': '15px', 'lineHeight': '19px', 'marginBottom': '5px', 'marginTop': '20px'}}>
+                <Block>
+                  <CakeHarvestBalance earningsSum={earningsSum}/>
+                  {account ? (
+                    <div>
+                      <Label>~${(eggPrice * earningsSum).toFixed(2)}</Label>
+                      <Sub>MIS to Settle</Sub>
+                    </div>
+                  ):(<Sub>MIS to Settle</Sub>)
+                  }
+                  
+                </Block>
+            </div>
+            <div style={{'display':'inline-block', 'paddingLeft': '15px', 'lineHeight': '19px', 'marginBottom': '5px'}}>
+                <Block>
+                  <CakeWalletBalance cakeBalance={cakeBalance} />
+                  {account ? (
+                    <div>
+                      <Label>~${(eggPrice * cakeBalance).toFixed(2)}</Label>
+                      <Sub>MIS in Wallet</Sub>
+                    </div>
+                  ):(<Sub>MIS in Wallet</Sub>)
+                  }
+                </Block>
+            </div>
+          </FlowCol>
+          </CardBody>
+
     </SvgHero>
     </div>
         <Wrapper>
+
+
         <Title>
           <span><FaMap/> Explore Artemis Earn</span>
         </Title>
@@ -146,6 +286,14 @@ const MoneyWheel: React.FC = () => {
 
         </Wrapper>
       </CardBody>
+
+
+
+
+        
+        
+ 
+
     </StyledCakeStats>
 
 
